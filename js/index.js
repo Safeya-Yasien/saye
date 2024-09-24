c2f16d9f5eefae2b986701aac5d2a9e2b86b4970
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-24 11:37:20 +0300

    fix: :bug: fix slick slider

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".slick").slick({
+    infinite: true,
+    speed: 100,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    rtl: true,
+    autoplay: true,
+  });
+});
+
+// $(".your-class").slick({
+//   slidesToShow: 3,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+//   dots: true,
+//   rtl: true, // Enable RTL
+// });
